eb839bc6f991ced53984906a18f5b8226d972820
anschrammh <anatole.schramm-henry@etu.udamail.fr>
2018-01-22 17:36:27 +0100

    [restAPI] Added api request

--- swarm2dev/standaloneModules/restApi/restApi.c
+++ swarm2dev/standaloneModules/restApi/restApi.c
@@ -3,12 +3,25 @@
 #include <string.h>
 
 #include "restApi.h"
+//Necessary to be send via http
+void write_json_http_header(char *buffer)
+{
+    http_write_header_code(buffer, HTTP_OK);
+    http_write_content_type(buffer, "application/json");
+    http_write_header_end(buffer);
+}
 
 void rest_api_exec(const char *restUrl, HTTP_QUERRY_TYPE querry_type, char *buffer)
 {
     int id = 0;
     char axes = ' ';
-    //We first check the querry typedef
+
+    JsonBuffer json;
+    write_json_http_header(buffer);
+    json_init(&json, buffer+strlen(buffer)/*json text doesn t overwrite our header*/, 100, JSON_MONOBLOC);//The size is hard coded, this needs to be changed
+
+
+    //We first check the querry type
     switch(querry_type)
     {
     case HTTP_QUERRY_TYPE_GET:
@@ -21,7 +34,7 @@ void rest_api_exec(const char *restUrl, HTTP_QUERRY_TYPE querry_type, char *buff
         else if (strncmp(restUrl, "motors/",7) == 0)
         {
             //We retrieve the motor id
-            sscanf(restUrl,"%d",&id);
+            sscanf(restUrl,"%*[a-z/]%d",&id);
         }
         //------------------------LASER------------------------------------
         else if (strcmp(restUrl, "laserrangefinders") == 0)
@@ -31,17 +44,17 @@ void rest_api_exec(const char *restUrl, HTTP_QUERRY_TYPE querry_type, char *buff
         else if (strncmp(restUrl, "laserrangefinders/", 18) == 0)
         {
             //We retrieve the laser id
-            sscanf(restUrl,"%d",&id);
+            sscanf(restUrl,"%*[a-z/]%d",&id);
         }
-        //---------------------TRAVEL-DISTANCE-----------------------------
+        //---------------------TRAVEL_DISTANCE-----------------------------
         else if (strcmp(restUrl, "traveldistances") == 0)
         {
 
         }
         else if (strncmp(restUrl, "traveldistances/", 16) == 0)
         {
-            //We retrieve the laser id
-            sscanf(restUrl,"%d",&id);
+            //We retrieve the traveldistances id
+            sscanf(restUrl,"%*[a-z/]%d",&id);
         }
         //----------------------ACCELEROMETER---------------------------------
         else if (strcmp(restUrl, "accelerations") == 0)
@@ -50,16 +63,26 @@ void rest_api_exec(const char *restUrl, HTTP_QUERRY_TYPE querry_type, char *buff
         }
         else if (strncmp(restUrl, "accelerations/", 14) == 0)
         {
-            sscanf(restUrl, "/%c", &axes);
+            sscanf(restUrl, "%*[a-z]/%c", &axes);
         }
-        //-------------------ROTARYENCODER------------------------
+        //-------------------ROTARY_ENCODER------------------------
         else if (strcmp(restUrl, "rotaryencoderpositions") == 0)
         {
 
         }
         else if (strncmp(restUrl, "rotaryencoderpositions/", 23) == 0)
         {
-            sscanf(restUrl, "%d", &id);
+            sscanf(restUrl, "%*[a-z/]%d", &id);
+        }
+        //--------------------STATUS------------------------------
+        else if (strcmp(restUrl, "batterylevel") == 0)
+        {
+
+        }
+        else //The resource wasn't found
+        {
+            http_write_header_code(buffer, HTTP_NOT_FOUND);
+            http_write_header_end(buffer);
         }
 
         break;
@@ -67,6 +90,8 @@ void rest_api_exec(const char *restUrl, HTTP_QUERRY_TYPE querry_type, char *buff
         break;
     default :
         //If we encounter an other querry type
+        http_write_header_code(buffer, HTTP_NOT_IMPLEMENTED);
+        http_write_header_end(buffer);
         break;
     }
 
